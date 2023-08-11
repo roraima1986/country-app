2d24d8e970687d24ae70227c3a5fa45f36a94c04
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-11 11:18:25 -0400

    Router Module y Paginas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { HomePageComponent } from './shared/pages/homePage/homePage.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**',
+    redirectTo:'home',
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule{
+
+}
---
+++ src/app/shared/pages/homePage/homePage.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  templateUrl: './homePage.component.html'
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
