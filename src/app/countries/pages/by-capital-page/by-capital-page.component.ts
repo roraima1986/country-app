016a8cff1593fc47eaacfd6c6e8bceb5f17e3c50
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-13 17:08:56 -0400

    Mostrar y ocultar el LoadingSpinner

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -8,7 +8,8 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByCapitalPageComponent implements OnInit {
 
-  public countries:Country[] = []
+  public countries:Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) { }
 
@@ -16,9 +17,12 @@ export class ByCapitalPageComponent implements OnInit {
   }
 
   searchByCapital(term:string):void{
+
+    this.isLoading = true;
     this.countriesService.searchCapital(term)
       .subscribe(countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
 
       console.log({term});
